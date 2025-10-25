70fca2db594e09b5b686ffd74fc4191e2b58f481
Leo-Hingpit <eolhingpit@gmail.com>
2025-10-25 16:25:28 +0800

    last na untana

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -7,11 +7,7 @@ export default function AdminDashboard() {
     <div>
       <h2 className="mb-4">Admin Dashboard</h2>
 
-      <div className="mb-3">
-        <Link className="btn btn-secondary me-2" to="/staff/orders">
-          View Orders
-        </Link>
-      </div>
+      
 
       <StaffItems /> {/* reuse your existing CRUD component */}
     </div>
